4f2312a4a293dc3c45808e6b840d6b31b88bc431
hwstar <hwstar@rodgers.sdcoxmail.com>
2013-09-04 09:00:35 -0700

    Update ptest.c
    Fix date

--- ptest.c
+++ ptest.c
@@ -1,7 +1,7 @@
 /*
  * ptest.c
  * 
- *  Copyright (C) 22013  Stephen Rodgers
+ *  Copyright (C) 2013  Stephen Rodgers
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
