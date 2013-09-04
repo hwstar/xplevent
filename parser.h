e639dba078bc1ef1611ff2abbe723e890929ea1f
hwstar <hwstar@rodgers.sdcoxmail.com>
2013-09-04 11:30:01 -0700

    Update parser.h

--- parser.h
+++ parser.h
@@ -121,7 +121,7 @@ Bool ParserPcodeGetValue(pcodeHeaderPtr_t ph, pcodePtr_t instr, String *pValue);
 Bool ParserPcodePutValue(pcodeHeaderPtr_t ph, pcodePtr_t instr, String value);
 int ParserExecPcode(pcodeHeaderPtr_t ph);
 int ParserParseHCL(ParseCtrlPtr_t this, int fileMode, const String str);
-String ParserMoveString(void newCtx, void *oldCtx, String oldStr);
+String ParserMoveString(void *newCtx, void *oldCtx, String oldStr);
 
 
 
