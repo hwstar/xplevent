e5f5c40b810395ae60e8334cf6f0160a5b4de452
hwstar <hwstar@rodgers.sdcoxmail.com>
2013-09-09 14:29:51 -0700

    Update db.h

--- db.h
+++ db.h
@@ -7,5 +7,6 @@ const String DBReadNVState(TALLOC_CTX *ctx, void *db, const String key);
 Bool DBWriteNVState(TALLOC_CTX *ctx, void *db, const String key, const String value);
 const String DBFetchScript(TALLOC_CTX *ctx, void *db, const String scriptName);
 const String DBFetchScriptName(TALLOC_CTX *ctx, void *db, const String tagSubAddr);
+Bool DBUpdateTrigLog(TALLOC_CTX *ctx, void *db, const String source, const String nvpairs);
 
 #endif
