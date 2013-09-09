837eaa2a67bddabcff26ace898a30852fdfe58dd
hwstar <hwstar@rodgers.sdcoxmail.com>
2013-09-09 10:02:21 -0700

    Update db.h

--- db.h
+++ db.h
@@ -1,4 +1,9 @@
+#ifndef DB_H
+#define DB_H
 
+void *DBOpen(String sqlitefile);
+void DBClose(void *db);
 const String DBReadNVState(TALLOC_CTX *ctx, void *db, const String key);
 Bool DBWriteNVState(TALLOC_CTX *ctx, void *db, const String key, const String value);
 
+#endif
