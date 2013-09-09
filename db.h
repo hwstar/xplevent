293584081b81d3991068b30e24a3edeee9e0acd1
hwstar <hwstar@rodgers.sdcoxmail.com>
2013-09-09 12:36:46 -0700

    Update db.h

--- db.h
+++ db.h
@@ -1,9 +1,11 @@
 #ifndef DB_H
 #define DB_H
 
-void *DBOpen(String sqlitefile);
+void *DBOpen(String file);
 void DBClose(void *db);
 const String DBReadNVState(TALLOC_CTX *ctx, void *db, const String key);
 Bool DBWriteNVState(TALLOC_CTX *ctx, void *db, const String key, const String value);
+const String DBFetchScript(TALLOC_CTX *ctx, void *db, const String scriptName);
+const String DBFetchScriptName(TALLOC_CTX *ctx, void *db, const String tagSubAddr);
 
 #endif
