a1dff079c4d4614c7348465ef8f8a2d8cc5f6188
hwstar <hwstar@rodgers.sdcoxmail.com>
2013-09-11 12:25:26 -0700

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,7 +1,6 @@
 
-
 #ifndef MONITOR_H
 #define MONITOR_H
-void MonitorPreForkSetup(String interface);
+void MonitorPreForkSetup(String interface, String instance_id);
 void MonitorRun(void);
 #endif
