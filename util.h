a81aa892b6364c2d672065caffc8741f769eb510
hwstar <hwstar@rodgers.sdcoxmail.com>
2013-09-12 16:09:05 -0700

    Update util.h

--- util.h
+++ util.h
@@ -2,7 +2,7 @@
 #define UTIL_H
 
 String *UtilFileReadString(TALLOC_CTX *ctx, String filename);
-int UtilFileWriteString(String filename, String str);
+Bool UtilFileWriteString(String filename, String str)
 static uint32_t UtilHash(const String key);
 String UtilMoveString(TALLOC_CTX *newCtx, String oldStr, int offset);
 String UtilStringCopy(String dest, const String src, int charsToCopy);
