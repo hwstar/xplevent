dfcc1c3bde275582449e629bb32abbf6f25a1be7
hwstar <hwstar@rodgers.sdcoxmail.com>
2013-09-12 15:43:27 -0700

    Update util.h

--- util.h
+++ util.h
@@ -7,7 +7,7 @@ static uint32_t UtilHash(const String key);
 String UtilMoveString(TALLOC_CTX *newCtx, String oldStr, int offset);
 String UtilStringCopy(String dest, const String src, int charsToCopy);
 pid_t UtilPIDRead(char *filename);
-int UtilPidWrite(String filename, pid_t pid);
+int UtilPIDWrite(String filename, pid_t pid);
 
 
 #endif
