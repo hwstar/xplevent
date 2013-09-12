f4d0de4c7826ad5b5f54d9361d87d92dfba23749
hwstar <hwstar@rodgers.sdcoxmail.com>
2013-09-12 11:48:09 -0700

    Update util.c

--- util.c
+++ util.c
@@ -1,5 +1,4 @@
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
@@ -22,29 +21,36 @@ String *UtilFileReadString(TALLOC_CTX *ctx, String filename)
   int fd; 
   struct stat s;
   void *buf;
-  String str;
+  String str = NULL;
   
   ASSERT_FAIL(filename);
   
   if((fd = open(filename, O_RDONLY)) < 0){
+    debug(DEBUG_UNEXPECTED,"File open error on %s", filename)
     return NULL;
   }
   
-  fstat(fd, &s);
+  if(fstat(fd, &s) < 0){
+    debug(DEBUG_UNEXPECTED,"File stat error on %s", filename)
+    return NULL;
+  }
+  
   
   if((buf = mmap(0, s.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == (void *) -1){
+    debug(DEBUG_UNEXPECTED,"mmap error")
     return NULL;
   }
    
   str = talloc_array(ctx, char, s.st_size + 1);
+  
+  MALLOC_FAIL(buf);
+  
   memcpy(s, buf, s.st_size);
   str[s.st_size] = 0;
   
   munmap(buf);
   
-  if(close(fd) < 0){
-    return NULL;
-  }
+  close(fd);
   
   return str;
   
