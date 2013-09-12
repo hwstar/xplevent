b54b630f65646663b8bdf5e61c99893fd23fa1c5
hwstar <hwstar@rodgers.sdcoxmail.com>
2013-09-12 08:46:05 -0700

    Update db.h

--- db.h
+++ db.h
@@ -9,5 +9,6 @@ const String DBFetchScript(TALLOC_CTX *ctx, void *db, const String scriptName);
 const String DBFetchScriptByTag(TALLOC_CTX *ctx, void *db, const String tagSubAddr);
 Bool DBUpdateTrigLog(TALLOC_CTX *ctx, void *db, const String source, const String schema, const String nvpairs);
 Bool DBUpdateHeartbeatLog(TALLOC_CTX *ctx, void *db, const String source);
+Bool DBIRScript(TALLOC_CTX *ctx, void *db, const String name, const String script);
 
 #endif
