ff246762cba27120ed539a63c1fab57cdf8009ff
hwstar <hwstar@rodgers.sdcoxmail.com>
2013-09-12 11:43:03 -0700

    Update util.c

--- util.c
+++ util.c
@@ -1,6 +1,5 @@
 
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
@@ -8,6 +7,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <talloc.h>
 #include "defs.h"
 #include "types.h"
 #include "notify.h"
@@ -17,28 +17,35 @@
 * Memory map a file
 */
 
-String *UtilMMFile(String filename)
+String *UtilFileReadString(TALLOC_CTX *ctx, String filename)
 {
   int fd; 
   struct stat s;
   void *buf;
+  String str;
   
   ASSERT_FAIL(filename);
   
-  if((fd = open(filename, O_RDONLY)) < 0)
-    return NULL;
-  
-  fstat(fd, &s);
-  
-  if((buf = mmap(0, s.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == (void *) -1)
-    return NULL;
-  
-  
-  if(close(fd) < 0){
-    munmap(buf);
+  if((fd = open(filename, O_RDONLY)) < 0){
     return NULL;
   }
   
-  return String buf;
+  fstat(fd, &s);
+  
+  if((buf = mmap(0, s.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == (void *) -1){
+    return NULL;
+  }
+   
+  str = talloc_array(ctx, char, s.st_size + 1);
+  memcpy(s, buf, s.st_size);
+  str[s.st_size] = 0;
+  
+  munmap(buf);
+  
+  if(close(fd) < 0){
+    return NULL;
+  }
+  
+  return str;
   
 }
