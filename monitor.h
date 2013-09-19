57143924121c3a854cd27e2af523180a5f2e1be4
hwstar <hwstar@rodgers.sdcoxmail.com>
2013-09-19 13:53:57 -0700

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,6 +1,9 @@
-
 #ifndef MONITOR_H
 #define MONITOR_H
 void MonitorPreForkSetup(String interface, String instance_id);
 void MonitorRun(void);
+void MonitorSendScript(TALLOC_CTX *ctx, int userSock, String theScript, String id);
+Bool MonitorRecvScript(MonRcvInfoPtr_t ri, String line);
+
+
 #endif
