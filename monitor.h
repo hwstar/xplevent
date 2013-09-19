3607048212fef4bf39eaf984fee3e8b418934c89
hwstar <hwstar@rodgers.sdcoxmail.com>
2013-09-19 13:57:19 -0700

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,5 +1,19 @@
 #ifndef MONITOR_H
 #define MONITOR_H
+
+
+typedef enum {RS_IDLE = 0, RS_WAIT_LINE, RS_FINISHED, RS_ERROR} rs_state_t;
+
+typedef struct MonRcvInfo_s {
+	String script;
+	unsigned scriptLen;
+	unsigned scriptBufSize;
+	unsigned scriptSizeLimit;
+	int userSock;
+	rs_state_t state;
+} MonRcvInfo_t;
+
+
 void MonitorPreForkSetup(String interface, String instance_id);
 void MonitorRun(void);
 void MonitorSendScript(TALLOC_CTX *ctx, int userSock, String theScript, String id);
