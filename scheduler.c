156244ec444023efadbc1f938bc2308f505f4c67
hwstar <hwstar@rodgers.sdcoxmail.com>
2013-09-20 15:28:01 -0700

    Update scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -54,43 +54,7 @@ static void SchedulerDo(TALLOC_CTX *ctx, SchedInfoPtr_t sch)
 	
 	/* walk the list */
 	for(skedlist = sch->list; skedlist; skedlist = skedlist->next){
-		debug(DEBUG_STATUS, "Scheduler entry %s = %s being considered\n",skedlist->name, skedlist->value);
-		strncpy(value,skedlist->value,99);
-		value[99] = 0;
-		/* point to the substrings for minute, hour, dom, month, and dow */
-		for( i = 0, vp = value ; i < 5; i++){
-			if(!*vp)
-				break;
-			while((*vp == ' ') || (*vp == 0x09)) /* get rid of any leading white space */
-				vp++;
-			strs[i] = vp; /* save pointer to beginning of substring */
-			while((*vp != ' ') && (*vp != 0x09) && (*vp != 0)) /* skip over substring */
-				vp++;
-			if(*vp)
-				*vp++ = 0; /* mark end of substring */
-		}
-		debug(DEBUG_STATUS, "i = %d, min = %s, hour = %s, mday=%s, mon=%s, wday=%s\n",i,
-			strs[0], strs[1], strs[2], strs[3], strs[4]); 
- 		if(i == 5){
-			if((*strs[0] != '*')&&(atoi(strs[0]) != tmnow.tm_min))
-				continue;
-			if((*strs[1] != '*')&&(atoi(strs[1]) != tmnow.tm_hour))
-				continue;
-			if((*strs[2] != '*')&&(atoi(strs[2]) != tmnow.tm_mday))
-				continue;
-			if((*strs[3] != '*')&&(atoi(strs[3]) != tmnow.tm_mon + 1))
-				continue;
-			if(atoi(strs[4]) == 7)
-				strs[4] = "0";
-			if((*strs[4] != '*')&&(atoi(strs[4]) != tmnow.tm_wday))
-				continue;
-			debug(DEBUG_STATUS, "Executing scheduler entry %s = %s\n", skedlist->name, skedlist->value);
 
-		}
-		else{
-			debug(DEBUG_UNEXPECTED,"Malformed scheduler entry in rpt.conf: %s = %s\n",
-				skedlist->name, skedlist->value);
-		}
 	}
 
 }
